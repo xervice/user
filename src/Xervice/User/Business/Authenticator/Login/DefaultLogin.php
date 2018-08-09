0e0ddf725486b736f4fe05195f55c03f007f1bfd
Mike Bertram <contact@mibexx.de>
2018-08-09 08:14:09 +0200

    * Bugfix no constructor in LoginPlugins

--- src/Xervice/User/Business/Authenticator/Login/DefaultLogin.php
+++ src/Xervice/User/Business/Authenticator/Login/DefaultLogin.php
@@ -6,25 +6,9 @@ namespace Xervice\User\Business\Authenticator\Login;
 
 use DataProvider\UserAuthDataProvider;
 use DataProvider\UserCredentialDataProvider;
-use Xervice\User\Business\Authenticator\UserCredentialProviderInterface;
 
 class DefaultLogin implements LoginInterface
 {
-    /**
-     * @var \Xervice\User\Business\Authenticator\UserCredentialProviderInterface
-     */
-    private $authenticator;
-
-    /**
-     * DefaultLogin constructor.
-     *
-     * @param \Xervice\User\Business\Authenticator\UserCredentialProviderInterface $authenticator
-     */
-    public function __construct(UserCredentialProviderInterface $authenticator)
-    {
-        $this->authenticator = $authenticator;
-    }
-
     /**
      * @param \DataProvider\UserAuthDataProvider $authDataProvider
      * @param \DataProvider\UserCredentialDataProvider $userCredentialDataProvider
